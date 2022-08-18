90b82c43cb62ea784442febad907cf3f7e7e55f2
Victor <victor_maznichenko@mail.ru>
2022-08-18 20:08:12 +0300

    верстка главной страницы, дробление на компоненты

---
+++ src/components/UI/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './button.module.scss'
+
+function Button({children, fill, className}) {
+  return (
+    <div className={`${styles.button} ${fill ? styles.fill : ''} ${className}`}>
+        {children}
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/UI/Option/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './option.module.scss'
+
+
+function Option({text, disabled, className}) {
+  return (
+    <button className={`${styles.option} ${className}`} type="button" disabled={disabled}>{text}</button>
+  )
+}
+
+export default Option
