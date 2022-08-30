daf0e9abe218e142f831b064f69f5a7aef056100
Victor <victor_maznichenko@mail.ru>
2022-08-30 08:59:13 +0300

    Много изменений... Начинаю переносить все на Redux

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,49 +1,64 @@
 import React from "react";
 
-import Header from "components/layout/Header";
 import PizzaCard from "components/UI/PizzaCard";
-import CategoryItem from "components/UI/CategoryItem";
+import Skeleton from "components/UI/PizzaCard/Skeleton";
 import Sort from "components/UI/Sort";
-import Footer from "components/layout/Footer";
 
+//Import styles
 import styles from "./home.module.scss";
+import Categories from "components/UI/Categories";
+import Pagination from './../../components/UI/Pagination/index';
+import { SearchContext } from 'App';
 
-function Home({ pizzasData }) {
-  const categoriesData = [
-    "Все",
-    "Мясные",
-    "Вегетарианская",
-    "Гриль",
-    "Острые",
-    "Закрытые",
-  ];
-  const CategoriesList = categoriesData.map((item, id) => (
-    <CategoryItem className={styles.categoryItem} text={item} key={id} />
-  ));
-
-  const PizzasList = pizzasData.map((item, id) => (
-    <PizzaCard className={styles.item} key={id} {...item} />
+function Home({
+  pizzasData,
+  isLoading,
+  categorySelect,
+  categoriesData,
+  sortType,
+  onChangeSort,
+  changeOrderSort,
+  sortOrder,
+  setCurrentPage
+}) {
+  const {searchValue} = React.useContext(SearchContext);
+  const PizzasList = pizzasData
+    .filter((obj) =>
+      obj.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+    )
+    .map((item, id) => (
+      <PizzaCard className={styles.item} key={"PizzaCard_" + id} {...item} />
+    ));
+  const SkeletonList = [...new Array(8)].map((_, id) => (
+    <Skeleton className={styles.item} key={"Skeleton_" + id} />
   ));
 
   return (
-    <>
-      <Header />
-      <main className="main">
-        <div className="container">
-          <div className="main__inner">
-            <nav className="main__nav">
-              <ul className="main__nav-filters filters">{CategoriesList}</ul>
-              <Sort />
-            </nav>
-            <section className="main__pizzas pizzas">
-              <h1 className="pizzas__title title">Все пиццы</h1>
-              <div className="pizzas__items">{PizzasList}</div>
-            </section>
-          </div>
+    <main className={styles.root}>
+      <div className="container">
+        <div className={styles.inner}>
+          <nav className={styles.nav}>
+            <Categories
+              categorySelect={categorySelect}
+              categoriesData={categoriesData}
+            />
+            <Sort
+              onChangeSort={onChangeSort}
+              value={sortType}
+              sortOrder={sortOrder}
+              changeOrderSort={changeOrderSort}
+            />
+          </nav>
+          <section className={styles.pizzas}>
+            <h1 className={`${styles.pizzasTitle} title`}>Все пиццы</h1>
+            <div className={styles.pizzasItems}>
+              {!isLoading ? PizzasList : SkeletonList}
+            </div>
+            <Pagination setCurrentPage={setCurrentPage} count={pizzasData.length} />
+          </section>
         </div>
-      </main>
-      <Footer />
-    </>
+      </div>
+    </main>
   );
 }
 
